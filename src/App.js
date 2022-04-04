0bfab19ff1e7d728d2718b037d1666621ecd05aa
Kate Kiatsiri <keidsiri@gmail.com>
2022-04-04 11:59:26 -0700

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Reacr from 'react';
+import ClassCounter from './components/ClassCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassCounter />
+    </div>
+  );
+}
+
+export default App;
